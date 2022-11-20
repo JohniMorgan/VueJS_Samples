aded5c8364151383f1dedcc53b1aedcd9fea028a
Eugeny Butov <zim-ka@mail.ru>
2022-11-20 17:04:28 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <MyComponent/>
+  </div>
+  
+</template>
+
+<script>
+import MyComponent from './components/MyComponent';
+
+export default {
+  name: 'App',
+  components: {
+    MyComponent
+  }
+}
+</script>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="root">
+        <label>
+            Введите email:
+            <input 
+                type="email" 
+                :value="email" 
+                @input="emailInput($event.target.value)"
+            />
+        </label>
+        <label>
+            Введите пароль:
+            <input type="password" :value="password" />
+        </label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyComponent',
+    data()  {
+        return {
+            email: 'example@mail.ru',
+            password: '12345678'
+        }
+    }, 
+    methods: {
+        emailInput(value) {
+            this.data = value
+        }
+    }
+}
+
+</script>
